715063355dcaec71e5d52ccc3cfc981bf6eb2b73
lucienn3 <lucienn3@gmx.de>
2014-01-07 18:20:58 -0500

    general settings in config

--- htdocs/config.inc.php
+++ htdocs/config.inc.php
@@ -7,7 +7,11 @@
 */
 
 // Security
-if(isset($main)) else DIE("No Access here!");
+if(!isset($main)) DIE("No Access here!");
+    
+// General
+define("PAGE_NAME","Webinterface");
+define("PAGE_DESC","This is a webinterface for Just Cause 2 Multiplayer.");
 
 // Language
 $lang = "en"; // en = english, ge = german
