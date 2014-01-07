48a557a99f1ff0858b4803fc4b243018a05ceddb
lucienn3 <lucienn3@gmx.de>
2014-01-07 17:06:50 -0500

    content pages

---
+++ htdocs/lib/content/login.php
@@ -0,0 +1,16 @@
+<?
+
+/*
+//////////////
+//  Config  //
+//////////////
+*/
+
+// Security
+if(isset($main)) else DIE("No Access here!");
+
+// Language
+$lang = "en"; // en = english, ge = german
+
+
+?>
