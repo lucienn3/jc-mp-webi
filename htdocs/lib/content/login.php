4b72c5cff30f58288db1c365a90398bc84698e45
lucienn3 <lucienn3@gmx.de>
2014-01-07 18:21:55 -0500

    login

--- htdocs/lib/content/login.php
+++ htdocs/lib/content/login.php
@@ -1,16 +1,44 @@
-<?
+<!DOCTYPE html>
+<html lang="<?php echo $lang; ?>">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo PAGE_DESC; ?>">
+    <meta name="author" content="TuaTim">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
 
-/*
-//////////////
-//  Config  //
-//////////////
-*/
+    <title><?php echo PAGE_NAME; ?></title>
 
-// Security
-if(isset($main)) else DIE("No Access here!");
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.css" rel="stylesheet">
 
-// Language
-$lang = "en"; // en = english, ge = german
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" role="form" method="POST">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
 
 
-?>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
