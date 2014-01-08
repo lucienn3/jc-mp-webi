c7c9031b2b7b6945dfe3457fafce02f3644d56ea
lucienn3 <lucienn3@gmx.de>
2014-01-08 17:57:53 -0500

    lang
    added lang

--- htdocs/lib/lang/de.php
+++ htdocs/lib/lang/de.php
@@ -3,11 +3,14 @@
 
 // Login
 define("LOGIN_HEAD_TEXT", "Login");
-define("LOGIN_PLACEHOLDER", "Passwort");
+define("LOGIN_PLACEHOLDER_USERNAME","Login-Namen");
+define("LOGIN_PLACEHOLDER_PASSWORD", "Passwort");
 define("LOGIN_SINGIN", "Einloggen");
 define("LOGIN_ERROR_EMPTY","Sie m&uuml;ssen ein Passwort eingeben.");
-define("LOGIN_ERROR_WRONG","Das Passwort ist falsch.");
+define("LOGIN_ERROR_WRONG_PW","Das Passwort ist falsch.");
+define("LOGIN_ERROR_WRONG_USER","Das Username ist falsch.");
 define("LOGIN_ERROR_SUCCESS","Sie sind nun eingeloggt. Sie werden weitergeleitet.");
+
 
 
 ?>
--- htdocs/lib/lang/en.php
+++ htdocs/lib/lang/en.php
@@ -3,10 +3,13 @@
 
 // Login
 define("LOGIN_HEAD_TEXT", "Please Sign In");
-define("LOGIN_PLACEHOLDER", "Password");
+define("LOGIN_PLACEHOLDER_USERNAME","Userlogin");
+define("LOGIN_PLACEHOLDER_PASSWORD", "Password");
 define("LOGIN_SINGIN", "Sign In");
 define("LOGIN_ERROR_EMPTY","Please insert a password.");
-define("LOGIN_ERROR_WRONG","Your password is wrong.");
+define("LOGIN_ERROR_WRONG_PW","Your password is wrong.");
+define("LOGIN_ERROR_WRONG_USER","Your username is wrong.");
 define("LOGIN_ERROR_SUCCESS","You´re now logged in.");
+
 
 ?>
