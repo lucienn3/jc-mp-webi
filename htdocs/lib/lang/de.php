cf45b91847f0a518ef09de8cb749eee976b97dee
lucienn3 <lucienn3@gmx.de>
2014-01-07 19:14:48 -0500

    lang update

---
+++ htdocs/lib/lang/de.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// Login
+define("LOGIN_HEAD_TEXT", "Login");
+define("LOGIN_PLACEHOLDER", "Passwort");
+define("LOGIN_SINGIN", "Einloggen");
+define("LOGIN_ERROR_EMPTY","Sie m&uuml;ssen ein Passwort eingeben.");
+define("LOGIN_ERROR_WRONG","Das Passwort ist falsch.");
+define("LOGIN_ERROR_SUCCESS","Sie sind nun eingeloggt. Sie werden weitergeleitet.");
+
+
+?>
--- htdocs/lib/lang/en.php
+++ htdocs/lib/lang/en.php
@@ -2,6 +2,11 @@
 
 
 // Login
+define("LOGIN_HEAD_TEXT", "Please Sign In");
+define("LOGIN_PLACEHOLDER", "Password");
 define("LOGIN_SINGIN", "Sign In");
+define("LOGIN_ERROR_EMPTY","Please insert a password.");
+define("LOGIN_ERROR_WRONG","Your password is wrong.");
+define("LOGIN_ERROR_SUCCESS","You´re now logged in.");
 
 ?>
