052453f7a5c5f1f1fbc6ce63d6f6220cec8d3f76
lucienn3 <lucienn3@gmx.de>
2014-01-07 19:15:52 -0500

    changed config
    changed my own lang
    for tests

--- htdocs/config.inc.php
+++ htdocs/config.inc.php
@@ -13,8 +13,21 @@ if(!isset($main)) DIE("No Access here!");
 define("PAGE_NAME","Webinterface");
 define("PAGE_DESC","This is a webinterface for Just Cause 2 Multiplayer.");
 
-// Language
-$lang = "en"; // en = english, ge = german
+// Login
+define("PASSWORD", "test1234");
 
+// Language
+$lang = "de"; // en = english, ge = german
+
+// Other things
+$p      = "index";  // the main page
+
+
+/*////////////////////
+// DO NOT CHANGE!   //
+////////////////////*/
+
+// session active variable
+$sactive = false;
 
 ?>
