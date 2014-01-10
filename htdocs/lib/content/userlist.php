3b0e4b6e61897a62d24bb0581a654d80f67d3eca
lucienn3 <lucienn3@gmx.de>
2014-01-09 20:43:15 -0500

    change userlist.php
    added link for creating new account and changed some stuff
    
    And it makes it possible to active and inactive accounts

--- htdocs/lib/content/userlist.php
+++ htdocs/lib/content/userlist.php
@@ -1,7 +1,22 @@
 <?php
+
+// Database
+$user = parse_ini_file(DIR."/lib/users.ini", TRUE);
+if(isset($_GET["a"]) && $_GET["a"] == "act") {
+    if(isset($_GET["id"]) && array_key_exists($_GET["id"], $user)) {
+        if($user[$_GET["id"]]["active"] == "0") {
+            $user[$_GET["id"]]["active"] = "1";
+            ini_write(DIR."/lib/users.ini", $user);
+        } else {
+            $user[$_GET["id"]]["active"] = "0";
+            ini_write(DIR."/lib/users.ini", $user);
+        }
+        sendto("index.php?p=userlist",0);
+    }
+}
+
 // INDEX PAGE
 // Serverlist
-$user = parse_ini_file(DIR."/lib/users.ini", TRUE);
 include getTempl("header");
 ?>
 <table class="table table-hover">
@@ -10,8 +25,8 @@ include getTempl("header");
     <th><?php echo USERLIST_PERMESSION; ?></th>
     <th><?php echo USERLIST_ACTIVE; ?></th>
     <th><?php echo USERLIST_ACTIONS; ?></th>
-    <th>&nbsp;</th>
-    <th>&nbsp;</th>
+    <th></th>
+    <th></th>
     </tr>
 <?php
     while (list($key, $val) = each($user)) {   
@@ -25,13 +40,14 @@ include getTempl("header");
             echo "<td>".USERLIST_ACTIVE_FALSE."</td>"; 
             $reactive = USERLIST_ACTIVE_TRUE_TURN;
         }
-        echo "<td><a href=\"\"><span class=\"glyphicon glyphicon-ok\"> ".$reactive."</span></a></td>";
-        echo "<td><a href=\"\"><span class=\"glyphicon glyphicon-wrench\"> ".USERLIST_CHANGE."</span></a></td>";
+        echo "<td><a href=\"index.php?p=userlist&id=".$key."&a=act\"><span class=\"glyphicon glyphicon-ok\"> ".$reactive."</span></a></td>";
+        echo "<td><a href=\"index.php?p=me&id=".$key."\"><span class=\"glyphicon glyphicon-wrench\"> ".USERLIST_CHANGE."</span></a></td>";
         echo "<td><a href=\"\"><span class=\"glyphicon glyphicon-tower \"> ".USERLIST_PERM."</span></a></td>";
         echo "</tr>";
     }
 ?>
 </table>
+<a href="index.php?p=adduser"><span class="glyphicon glyphicon-plus"></span> <?php echo USERLIST_ADD; ?></a>
 <?php
 include getTempl("fooder");
 ?>
