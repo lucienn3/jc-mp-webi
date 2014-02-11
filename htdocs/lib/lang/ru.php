0f6d3d3d1f134454bdc3889d5d306eb049a9d69d
lucienn3 <lucienn3@gmx.de>
2014-02-11 16:17:25 -0500

    Russion Lang
    Thank you to Esamir

---
+++ htdocs/lib/lang/ru.php
@@ -0,0 +1,73 @@
+﻿<?php
+
+
+// Login
+define("LOGIN_HEAD_TEXT", "Пожалуйста, авторизуйтесь");
+define("LOGIN_PLACEHOLDER_USERNAME","Имя пользователя");
+define("LOGIN_PLACEHOLDER_PASSWORD", "Пароль");
+define("LOGIN_SINGIN", "Войти");
+define("LOGIN_ERROR_EMPTY","Введите пароль.");
+define("LOGIN_ERROR_WRONG_PW","Неверный пароль.");
+define("LOGIN_ERROR_WRONG_USER","Неверный логин.");
+define("LOGIN_ERROR_SUCCESS","Вы успешно авторизовались.");
+
+// Navigation
+define("NAV_INDEX","Список серверов");
+define("NAV_ADD","Добавить сервер");
+define("NAV_ADMIN_USER","Список пользователей");
+define("NAV_ADMIN_DROP_TITLE","Пользователь");
+define("NAV_ADMIN_DROP_ADMIN","Вы являетесь администраором");
+define("NAV_ADMIN_DROP_PASSWORD","Изменить пароль");
+define("NAV_ADMIN_DROP_LOGOUT","Выйти");
+define("NAV_ADMIN_SERVER","Управление сервером");
+
+// Filesystem Error
+ define("FILESYSTEM_ERROR_NOREAD", "Следующие файлы невозможно прочитать, проверьте доступ: ");
+ define("FILESYSTEM_ERROR_NOEXECUTE", "Следующие файлы не являются исполняемыми, проверьте права: ");
+
+// Userlist
+define("USERLIST_NAME","Имя пользователя");
+define("USERLIST_PERMESSION","Права доступа");
+define("USERLIST_ACTIVE","Активен?");
+define("USERLIST_ACTIVE_TRUE","активен");
+define("USERLIST_ACTIVE_FALSE","отключен");
+define("USERLIST_ACTIONS","действия");
+define("USERLIST_ACTIVE_TRUE_TURN","активировать");
+define("USERLIST_ACTIVE_FALSE_TURN","деактивировать");
+define("USERLIST_CHANGE","изменить пароль");
+define("USERLIST_PERM","изменить права");
+define("USERLIST_DEL","удалить");
+define("USERLIST_ADD","Добавить пользователя");
+
+// Password change
+define("PASSCH_USERMANE","Имя пользователя");
+define("PASSCH_OLDPW","Старый пароль");
+define("PASSCH_NEWPW","Новый пароль");
+define("PASSCH_NEWPWA","Повторите новый пароль");
+define("PASSCH_BUTTON","Изменить пароль");
+
+// add User
+define("ADDUSER_USERMANE","Имя пользователя");
+define("ADDUSER_NEWPW","Пароль");
+define("ADDUSER_NEWPWA","Повторите пароль");
+define("ADDUSER_ADMIN","Администратор?");
+define("ADDUSER_BUTTON","Создать пользователя");
+
+// Register
+define("REG_ERROR_WRONG_USER","Имя пользователя уже занято.");
+define("REG_ERROR_WRONG_PASS","Ваши пароли не совпадают, проверьте введённые данные.");
+
+// delte User
+define("DELLUSER_CHECK","удалить этого пользователя.");
+define("DELLUSER_BUTTON","Подтвердить");
+
+// Serverlist
+define("SLIST_STATUS","Онлайн?");
+define("SLIST_NAME","Имя");
+define("SLIST_PORT","Порт");
+define("SLIST_PLAYERS","Игроки");
+define("SLIST_PUBLIC","Публичный");
+define("SLIST_ACTIONS","Действия");
+
+
+?>
